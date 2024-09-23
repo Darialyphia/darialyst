e57ad28600c28445bde5fea34689fcc2e7c96a60
Loic Pennequin <lpennequin@samsys.fr>
2024-09-23 13:35:50 +0200

    fix unequip artifact effect not triggering destroy events

--- packages/sdk/src/card/actions/unequip-artifact.card-action.ts
+++ packages/sdk/src/card/actions/unequip-artifact.card-action.ts
@@ -4,7 +4,7 @@ export class UnequipArtifactCardAction extends CardAction<'unequip_artifact'> {
   protected async executeImpl() {
     const artifacts = this.getEquipedArtifacts(this.action.params.artifact);
     artifacts.forEach(artifact => {
-      artifact.player.unequipArtifact(artifact.id);
+      artifact.destroy();
     });
 
     return noop;
