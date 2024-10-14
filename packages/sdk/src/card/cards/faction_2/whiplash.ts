033252228a216574eb4a74eda961ffbac1417a33
Loic Pennequin <lpennequin@samsys.fr>
2024-10-14 13:49:37 +0200

    trying to fix whiplash :pepehands:

--- packages/sdk/src/card/cards/faction_2/whiplash.ts
+++ packages/sdk/src/card/cards/faction_2/whiplash.ts
@@ -43,13 +43,33 @@ export const f2Whiplash = defineSerializedBlueprint({
                 executionContext: 'while_on_board',
                 actions: [
                   {
-                    type: 'vulnerable',
+                    type: 'add_effect',
                     params: {
                       filter: { candidates: [], random: false },
                       execute: 'now',
                       activeWhen: { candidates: [], random: false },
                       duration: 'always',
-                      stacks: { type: 'fixed', params: { value: 1 } }
+                      stacks: { type: 'fixed', params: { value: 1 } },
+                      unit: {
+                        candidates: [[{ type: 'is_self', params: { not: false } }]],
+                        random: false
+                      },
+                      effect: {
+                        executionContext: 'while_on_board',
+                        actions: [
+                          {
+                            type: 'vulnerable',
+                            params: {
+                              filter: { candidates: [], random: false },
+                              execute: 'now',
+                              activeWhen: { candidates: [], random: false },
+                              duration: 'always',
+                              stacks: { type: 'fixed', params: { value: 1 } }
+                            }
+                          }
+                        ]
+                      },
+                      linkToCard: false
                     }
                   }
                 ]
