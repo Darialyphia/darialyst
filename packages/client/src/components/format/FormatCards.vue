56f8b027bb52dd3ce39f7df51ef8d1ac3dea4975
loicpennequin <lo.pennequin@gmail.com>
2024-12-27 14:37:02 +0100

    allow to create duplicate of cards

--- packages/client/src/components/format/FormatCards.vue
+++ packages/client/src/components/format/FormatCards.vue
@@ -24,8 +24,6 @@ const editedCards = computed(() =>
 const isEdited = (card: GenericSerializedBlueprint) =>
   editedCards.value.some(c => c.id === card.id);
 
-const standardCards = computed(() => Object.values(CARDS));
-
 const isCardsModalOpened = ref(false);
 const selectedCardId = ref<Nullable<string>>(null);
 const selectedCard = computed(() => {
@@ -40,15 +38,9 @@ const addCard = (card: GenericSerializedBlueprint) => {
   selectedCardId.value = card.id;
 };
 
-const search = ref('');
-const filteredCards = computed(() =>
-  standardCards.value.filter(c =>
-    c.name.toLowerCase().includes(search.value.toLocaleLowerCase())
-  )
-);
-
-const existingCardsList = useVirtualList(filteredCards, { itemHeight: 82, overscan: 5 });
 const validator = useFormatValidator();
+const isCreateCardPopoverOened = ref(false);
+const isDuplicateCardModalOpened = ref(false);
 </script>
 
 <template>
@@ -81,34 +73,82 @@ const validator = useFormatValidator();
         </li>
       </ul>
       <div class="flex items-center gap-2">
-        <UiButton
-          type="button"
-          class="primary-button"
-          is-inline
-          left-icon="material-symbols:add"
-          @click="
-            () => {
-              const id = nanoid(6);
-              format.cards[id] = {
-                id,
-                name: '',
-                collectable: true,
-                keywords: [],
-                relatedBlueprintIds: [],
-                tags: [],
-                kind: CARD_KINDS.MINION,
-                rarity: RARITIES.COMMON,
-                effects: [],
-                targets: { min: 0, targets: [] },
-                cellHighlights: [],
-                sounds: {}
-              } as any;
-              selectedCardId = id;
+        <PopoverRoot v-model:open="isCreateCardPopoverOened">
+          <InteractableSounds>
+            <PopoverTrigger as-child>
+              <UiButton
+                type="button"
+                class="primary-button"
+                is-inline
+                left-icon="material-symbols:add"
+                right-icon="tdesign:caret-down"
+                @click="isCreateCardPopoverOened = true"
+              >
+                Make New Card
+              </UiButton>
+            </PopoverTrigger>
+          </InteractableSounds>
+
+          <PopoverAnchor />
+
+          <PopoverPortal>
+            <PopoverContent as-child :collision-padding="20" :align-offset="20">
+              <div class="flex flex-col primary-button">
+                <UiButton
+                  class="rounded-0 w-full"
+                  @click="
+                    () => {
+                      isCreateCardPopoverOened = false;
+                      const id = nanoid(6);
+                      format.cards[id] = {
+                        id,
+                        name: '',
+                        collectable: true,
+                        keywords: [],
+                        relatedBlueprintIds: [],
+                        tags: [],
+                        kind: CARD_KINDS.MINION,
+                        rarity: RARITIES.COMMON,
+                        effects: [],
+                        targets: { min: 0, targets: [] },
+                        cellHighlights: [],
+                        sounds: {}
+                      } as any;
+                      selectedCardId = id;
+                    }
+                  "
+                >
+                  Empty card
+                </UiButton>
+                <UiButton
+                  class="rounded-0 w-full"
+                  @click="
+                    () => {
+                      isCreateCardPopoverOened = false;
+                      isDuplicateCardModalOpened = true;
+                    }
+                  "
+                >
+                  From existing card
+                </UiButton>
+              </div>
+            </PopoverContent>
+          </PopoverPortal>
+        </PopoverRoot>
+        <ExistingCardsModal
+          v-model:isOpened="isDuplicateCardModalOpened"
+          :is-card-disabled="card => isEdited(card)"
+          @select="
+            card => {
+              const clone = JSON.parse(JSON.stringify(card));
+              clone.id = nanoid(6);
+              clone.sounds ??= {};
+              clone.name += ' (copy)';
+              format.cards[clone.id] = clone;
+              selectedCardId = clone.id;
             }
           "
-        >
-          Make New Card
-        </UiButton>
+        />
       </div>
 
       <h3 class="mt-4">Edited Cards</h3>
@@ -148,45 +188,11 @@ const validator = useFormatValidator();
         Edit Card
       </UiButton>
 
-      <UiModal v-model:is-opened="isCardsModalOpened" title="Select a card">
-        <UiTextInput
-          id="card-search"
-          v-model="search"
-          placeholder="Search for a card"
-          left-icon="material-symbols:search"
-          class="mb-4"
-        />
-        <div class="overflow-hidden">
-          <div
-            class="existing-cards fancy-scrollbar"
-            v-bind="existingCardsList.containerProps"
-          >
-            <ul v-bind="existingCardsList.wrapperProps.value">
-              <li v-for="card in existingCardsList.list.value" :key="card.index">
-                <UiButton
-                  type="button"
-                  class="ghost-button"
-                  :disabled="isEdited(card.data)"
-                  @click="
-                    () => {
-                      addCard(card.data);
-                      isCardsModalOpened = false;
-                    }
-                  "
-                >
-                  <CardSprite :sprite-id="card.data.spriteId" class="sprite" />
-                  {{ card.data.name }}
-                </UiButton>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <footer class="flex justify-end">
-          <UiButton class="error-button" @click="isCardsModalOpened = false">
-            Cancel
-          </UiButton>
-        </footer>
-      </UiModal>
+      <ExistingCardsModal
+        v-model:isOpened="isCardsModalOpened"
+        :is-card-disabled="card => isEdited(card)"
+        @select="addCard"
+      />
     </section>
 
     <section>
@@ -247,8 +253,5 @@ section {
 .sprite {
   aspect-ratio: 1;
   width: 64px;
-}
-.existing-cards {
-  height: 60dvh;
 }
 </style>
